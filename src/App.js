f69b3581b416cd7c2087abfcbc927d1c2cd07c72
authcornelius <corneliusedos@gmail.com>
2024-01-24 10:08:42 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='container'>Hello Zumera</h1>
+      <h1 className='container'>Hello Zumera invest</h1>
     </div>
   );
 }
